89093de211b2cbb3c94c01584c76a619afcc3d1b
Jesus Benjamin Escacena <jesubes@gmail.com>
2022-11-06 13:20:05 -0300

    sin la implementacion de router

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,26 @@
+
+import React, { useState } from "react";
+import "./button.css"
+
+
+function Button ( props ) {
+    const [ colorState, setColorState ] = useState( {backgroundColor: props.color })
+
+
+    function handleClick() {
+        console.log("click")
+        // colorState = { bakcgrounColor: "#fff" }
+        setColorState ( { backgroundColor: "#A2EAE2" } )
+    }
+
+
+
+    return (
+        <button onClick={handleClick} style={ colorState } className="btn">
+            { props.children }
+        </button> 
+    )
+}
+
+
+export default Button
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Button from "../Button/Button"
 import './item.css'
 
+
+
 function Item(props){
-    
+ 
     return (
         <div className="card">
             <div>{ props.title }</div>
             <img src={props.img} alt="imagen" width="280px"></img>
+            <Button color="#F1C43B">
+               comprar
+            </Button>
         </div>
     )
 }
